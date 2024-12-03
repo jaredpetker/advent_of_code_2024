3e31d793113f735f53bbf32dc7c99df82819eb74
Jared Petker <jared@veloversa.com>
2024-12-02 23:06:38 -0800

    feat(day 3): complete

---
+++ src/bin/day_3/main.rs
@@ -0,0 +1,82 @@
+use regex::{Captures, Regex};
+use std::sync::LazyLock;
+
+static RE_INSTRUCTIONS: LazyLock<Regex> = LazyLock::new(
+    || Regex::new(r"(?<mul>mul\(\d+,\d+\))|(?<do>do\(\))|(?<dont>don't\(\))"
+    ).unwrap()
+);
+
+static RE_MUL: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"mul\((\d+),(\d+)\)").unwrap()
+);
+
+enum Instruction {
+    Mul(usize, usize),
+    Do,
+    Dont,
+}
+
+impl<'a> From<Captures<'a>> for Instruction {
+    fn from(captures: Captures<'a>) -> Self {
+        if let Some(mul) = captures.name("mul") {
+            let Some(mul_captures) = RE_MUL.captures(mul.as_str()) else {
+                panic!("Invalid 'mul' instruction: {}", mul.as_str());
+            };
+            let (_, [left, right]) = mul_captures.extract();
+            Instruction::Mul(left.parse().unwrap(), right.parse().unwrap())
+        } else if captures.name("do").is_some() {
+            Instruction::Do
+        } else if captures.name("dont").is_some() {
+            Instruction::Dont
+        } else {
+            panic!("Invalid instruction: {}", captures.get(0).unwrap().as_str());
+        }
+    }
+}
+
+struct Computer<'a> {
+    memory: &'a str,
+}
+
+impl<'a> Computer<'a> {
+    fn find_instructions(&self) -> Vec<Instruction> {
+        RE_INSTRUCTIONS
+            .captures_iter(self.memory)
+            .map(|captures| captures.into())
+            .collect()
+    }
+
+    fn run_instructions(&self, instructions: &Vec<Instruction>, enable_dos_and_donts: bool) -> usize {
+        let mut enable_mul = true;
+        instructions.iter().filter_map(|instruction| {
+            match instruction {
+                Instruction::Mul(left, right) => {
+                    if !enable_dos_and_donts || enable_mul {
+                        return Some(left * right);
+                    }
+                }
+                Instruction::Do => {
+                    enable_mul = true;
+                }
+                Instruction::Dont => {
+                    enable_mul = false;
+                }
+            };
+            return None;
+        }).sum()
+    }
+}
+
+fn main() {
+    let input = include_str!("input");
+    let computer = Computer {
+        memory: input
+    };
+
+    let instructions = computer.find_instructions();
+
+    let output_without_dos_and_donts = computer.run_instructions(&instructions, false);
+    println!("Output (disabled dos/donts): {}", output_without_dos_and_donts);
+
+    let output_with_dos_and_donts = computer.run_instructions(&instructions, true);
+    println!("Output (enabled dos/donts): {}", output_with_dos_and_donts);
+}
