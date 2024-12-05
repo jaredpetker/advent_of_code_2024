e3ca4d0fde322750fecf61cff15d258cd2f4b46a
Jared Petker <jared@veloversa.com>
2024-12-04 19:16:27 -0800

    refactor(day 2): further refactoring / cleanup to day 2

--- src/bin/day_2/main.rs
+++ src/bin/day_2/main.rs
@@ -7,21 +7,19 @@ enum LevelDirection {
     Unknown,
 }
 type Level = usize;
-struct Report {
-    levels: Vec<Level>,
-}
+struct Report(Vec<Level>);
 
 impl Report {
     fn is_safe(&self, adjacent_levels_tolerance_range: Range<Level>, skip_level_index: Option<usize>) -> bool {
         let mut level_direction = LevelDirection::Unknown;
-        for (i, level) in self.levels.iter().enumerate().skip(1) {
-            let mut last_level = self.levels[i - 1];
+        for (i, level) in self.0.iter().enumerate().skip(1) {
+            let mut last_level = self.0[i - 1];
             if let Some(skip_level_index) = skip_level_index {
                 if i == skip_level_index {
                     continue;
                 } else if i - 1 == skip_level_index {
                     if skip_level_index > 0 {
-                        last_level = self.levels[i - 2];
+                        last_level = self.0[i - 2];
                     } else {
                         continue;
                     }
@@ -43,7 +41,7 @@ impl Report {
 
 
     fn is_safe_with_bad_level(&self, adjacent_levels_tolerance_range: Range<Level>) -> bool {
-        let mut range_to_check = 0..self.levels.len();
+        let mut range_to_check = 0..self.0.len();
         range_to_check
             .any(|i| self.is_safe(adjacent_levels_tolerance_range.clone(), Some(i)))
     }
@@ -51,31 +49,34 @@ impl Report {
 
 impl From<&str> for Report {
     fn from(value: &str) -> Self {
-        Report {
-            levels: value.split_whitespace().map(|level| level.parse::<Level>().unwrap()).collect()
-        }
+        Report(value.split_whitespace().map(|level| level.parse::<Level>().unwrap()).collect())
     }
 }
 
-struct Reports {
-    reports: Vec<Report>,
-}
+struct Reports(Vec<Report>);
 
 impl Reports {
     fn count_safe_reports(&self, adjacent_levels_tolerance_range: Range<Level>, allow_bad_level: bool) -> usize {
         if !allow_bad_level {
-            self.reports.iter().filter(|report| report.is_safe(adjacent_levels_tolerance_range.clone(), None)).count()
+            self.0.iter().filter(|report| report.is_safe(adjacent_levels_tolerance_range.clone(), None)).count()
         } else {
-            self.reports.iter().filter(|report| report.is_safe_with_bad_level(adjacent_levels_tolerance_range.clone())).count()
+            self.0.iter().filter(|&Report(levels)| {
+                let mut range = 0..levels.len();
+                range.any(|i|{
+                    let split = levels.split_at(i);
+                    let mut check_vec = Vec::new();
+                    check_vec.extend(split.0);
+                    check_vec.extend(split.1);
+                    Report(check_vec).is_safe_with_bad_level(adjacent_levels_tolerance_range.clone())
+                })
+            }).count()
         }
     }
 }
 
 impl From<&str> for Reports {
     fn from(value: &str) -> Self {
-        Reports {
-            reports: value.lines().map(|line| Report::from(line)).collect()
-        }
+        Reports(value.lines().map(|line| Report::from(line)).collect())
     }
 }
 
